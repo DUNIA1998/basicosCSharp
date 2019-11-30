9935a3913db3afa1bb7958000d454d77f4697afa
DUNIA1998 <57006538+DUNIA1998@users.noreply.github.com>
2019-11-30 09:53:58 -0600

    Arreglos

--- basicosCSharp/basicosCSharp/Program.cs
+++ basicosCSharp/basicosCSharp/Program.cs
@@ -7,7 +7,7 @@ namespace basicosCSharp
         static void Main(string[] args)
         {
             
-            Console.ReadLine();
+       
 
             int añoActual = Convert.ToInt32("2019");
             double numeroPI = 3.141592;
@@ -45,10 +45,18 @@ namespace basicosCSharp
             bool comparacionIgual = comparacionY == comparacionO;
             bool valorNegado = !comparacionIgual;
 
+            string[] nombres = new string[10];
+            nombres[4] = "Ruben";
+            string[] diasDeLaSemana = { "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo" };
+            Console.WriteLine("La semana tiene {0} días", diasDeLaSemana.Length);
+            int[] numerosPrimos = { 1, 2, 3, 5, 7, 11 };
+            bool[] celdasMarcadas = new bool[16];
 
 
 
-            Console.WriteLine();
+
+
+            Console.WriteLine(primerNumero);
             Console.ReadLine();
 
 
