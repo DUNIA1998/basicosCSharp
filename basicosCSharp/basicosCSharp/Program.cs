43800237598e225d497365d40319851b055ece03
DUNIA1998 <57006538+DUNIA1998@users.noreply.github.com>
2019-11-30 10:01:14 -0600

    Ciclo for

--- basicosCSharp/basicosCSharp/Program.cs
+++ basicosCSharp/basicosCSharp/Program.cs
@@ -79,6 +79,14 @@ namespace basicosCSharp
             } while (rondasFaltantes > 0);
 
 
+            string[] diasSemanales = { "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo" };
+            for (int dia = 0; dia < 7; dia++)
+            {
+                Console.WriteLine("{0} es el número {1} de la semana", diasSemanales[dia], dia + 1);
+            }
+
+
+
 
 
 
