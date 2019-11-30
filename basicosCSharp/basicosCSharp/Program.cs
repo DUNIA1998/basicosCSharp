386e37ff25e75d6b9ed7bdfc113bfc479b51794a
DUNIA1998 <57006538+DUNIA1998@users.noreply.github.com>
2019-11-30 09:28:16 -0600

    Se trabaja con variables basicas

--- basicosCSharp/basicosCSharp/Program.cs
+++ basicosCSharp/basicosCSharp/Program.cs
@@ -6,8 +6,27 @@ namespace basicosCSharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
             Console.ReadLine();
+
+            int añoActual = Convert.ToInt32("2019");
+            double numeroPI = 3.141592;
+            Console.WriteLine("Valores:{0} -- {1}", añoActual, numeroPI);
+            decimal montoFactura = 94593.34m + 0.07m;
+            float impuestoVenta = 9239.04f;
+            Console.WriteLine("Valores:{0} -- {1}", montoFactura, impuestoVenta);
+            Char letraInicial = 'P';
+            string nombreCurso = letraInicial + "rogramación 11";
+            Console.WriteLine("Este es el curso {0}", nombreCurso);
+            bool boolVerdadero = true;
+            bool boolFalso = false;
+            Console.WriteLine("Variables boleanas:{0} y {1}", boolVerdadero, boolFalso);
+
+
+
+
+
+
         }
     }
 }
